24c9d7e123d83388260111a2e30fe60e98a7bd30
gema <gematravel10@gmail.com>
2019-02-15 22:50:58 +0700

    penambahan template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Gema antika hariadi"
+    user = {'name': 'gema antika hariadi',
+            'address': 'pandeyan umbulharjo',
+            'age': '21',
+            'hobby': 'doing this'}
+    post=[
+        {
+            'author': {'username': 'ennu intan iksan', 'age': '19'},
+            'have' : {'title': 'the strongest girl', 'hair': 'slim'}
+        },
+        {
+            'author': {'username': 'susanti auliya'},
+            'have' : {'title': 'the beutiful sound', 'hair': 'long'}
+        }
+    ]
+    return render_template('index.html', title='Home', tampilan='cakep bener', user=user, post=post)
